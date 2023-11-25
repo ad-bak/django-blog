8d57f5406dadb8ce7f47199fa7a68c1351a89397
Adnan <adbak.dev@gmail.com>
2023-11-25 23:19:18 +0100

    posts

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.starting_page, name="starting_page"),
-    path("posts", views.posts, name="post_page"),
+    path("posts", views.posts, name="posts_page"),
     path("posts/<slug:slug>", views.post_detail, name="post-detail-page"),
 ]
