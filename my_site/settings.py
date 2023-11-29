2443073ed6f57ab130bc0fa0f5fd1e2d1a82abf1
Adnan <adbak.dev@gmail.com>
2023-11-30 00:35:47 +0100

    database

--- my_site/settings.py
+++ my_site/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-_5a$$g^+18_+3ow_&f7bl7hez!b!+f1zwryd5jqzw59ge*f&66"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
